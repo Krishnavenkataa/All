e7ef29e4e5ac99db7f3544624d847cc8f6ecc2bc
Krishna <krishnackp051998@gmail.com>
2024-01-27 12:49:36 +0530

    first commit

---
+++ Practise.py
@@ -0,0 +1,28 @@
+'''import calendar
+import time
+def ProfileCreation():
+    Name = input("Name: ")
+    print("LOADING...")
+    time.sleep(1)
+    Age = input("Age: ")
+    print("LOADING...")
+    time.seep(1)
+    ProfileAns = input("This matches no profiles. Create profile? ")
+    if ProfileAns.lower() == 'yes':
+        print("Creating profile...")
+    elif ProfileAns.lower() == 'no':
+        print("Creating profile anyway...")
+    else:
+        print("yes or no answer.")
+        ProfileCreation()
+ProfileCreation()'''
+
+'''def fun():
+    print('hello')
+    print('hi')
+    return 'dsafds'
+    print('bye')
+l = fun()
+print(l)
+print('ramsssss')'''
+
